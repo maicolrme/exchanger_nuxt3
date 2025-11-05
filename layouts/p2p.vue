0f9c195f5a59e0313a111b8fe874e87d91a9b09d
Maicol Romero <michael12@hotmail.es>
2025-11-05 05:35:57 +0100

    Update p2p.vue

--- layouts/p2p.vue
+++ layouts/p2p.vue
@@ -276,12 +276,7 @@
           </svg>
           <span class="text-xs" :class="{ 'text-yellow-400': $route.path === '/markets', 'text-gray-400': $route.path !== '/markets' }">Mercados</span>
         </NuxtLink>
-        <NuxtLink to="/staking" class="flex flex-col items-center gap-1 bottom-nav-indicator" :class="{ 'text-yellow-400 active': $route.path.startsWith('/staking'), 'text-gray-400': !$route.path.startsWith('/staking') }">
-          <svg class="w-6 h-6" :class="{ 'text-yellow-400': $route.path.startsWith('/staking'), 'text-gray-400': !$route.path.startsWith('/staking') }" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 17v-8a2 2 0 012-2h12a2 2 0 012 2v8a2 2 0 01-2 2H6a2 2 0 01-2-2zm4-6h8m-8 4h6"></path>
-          </svg>
-          <span class="text-xs" :class="{ 'text-yellow-400': $route.path.startsWith('/staking'), 'text-gray-400': !$route.path.startsWith('/staking') }">Staking</span>
-        </NuxtLink>
+        
         <NuxtLink to="/p2p" class="flex flex-col items-center gap-1 bottom-nav-indicator" :class="{ 'text-yellow-400 active': $route.path.startsWith('/p2p'), 'text-gray-400': !$route.path.startsWith('/p2p') }">
           <svg class="w-6 h-6" :class="{ 'text-yellow-400': $route.path.startsWith('/p2p'), 'text-gray-400': !$route.path.startsWith('/p2p') }" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path>
